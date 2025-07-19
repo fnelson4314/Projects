2597daf64f0a9157027154d4c90199684b869786
fnelson4314 <fnelson4314@gmail.com>
2025-07-19 17:44:01 -0400

    Deleted public npm folder

---
+++ SpringbootProject/src/main/java/com/fosternelson/SpringbootProject/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.fosternelson.SpringbootProject;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+
+    @GetMapping(path = "/hello")
+    public String helloWorld() {
+        return "Hello World";
+    }
+}
